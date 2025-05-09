464e436ed4e603abaa448ce195b7afe3a6c7a387
5hrutika <shrutika.gaikwad@nonstopio.com>
2025-05-09 16:44:43 +0530

    Refactor registration component: implement loading state during user registration, enhance HTML structure for better loading indication, and update component imports for standalone functionality.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,   
+  imports: [RouterModule,CommonModule,FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   user:{email:string,password:string} = {email:"",password:""}
+  loading:boolean = false;
   constructor(private userService:UserService,private router:Router){}
   onSubmit(){
+    this.loading = true;
     this.userService.register(this.user).subscribe((data)=>{
-      console.log(data);
+     
+     setTimeout(() => {
+      this.loading = false;
       this.router.navigate(['/dashboard']);
+
+     }, 2000);
+      console.log(data);
     })
   }
 
